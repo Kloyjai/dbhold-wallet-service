69975bd11f38e74e162b24f07d464aa39d9cca7f
DESKTOP-GHK4PFM\Dev <kloy02jai@gmail.com>
2019-03-22 15:13:12 +0700

    update contents

--- src/main/java/com/oauth/social/oauthsocial/OauthSocialApplication.java
+++ src/main/java/com/oauth/social/oauthsocial/OauthSocialApplication.java
@@ -2,8 +2,12 @@ package com.oauth.social.oauthsocial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.oauth.social.oauthsocial.config.AppProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class OauthSocialApplication {
 
 	public static void main(String[] args) {
