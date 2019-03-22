34baecdb8fe6fb9a9fbbcd0793234e8d771c5915
DESKTOP-GHK4PFM\Dev <kloy02jai@gmail.com>
2019-03-22 11:53:10 +0700

    initial

---
+++ src/main/java/com/oauth/social/oauthsocial/OauthSocialApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.social.oauthsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSocialApplication.class, args);
+	}
+
+}
